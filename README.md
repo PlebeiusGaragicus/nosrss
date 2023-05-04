3541fd1ab91a675bc499d74dfb1e9d25398084a7
plebg <plebeiusgaragicus@gmail.com>
2023-05-03 17:38:33 -0700

    dev branch

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 > It scrapes the thing so it can send it to the... oh, you know what I mean..!
 
 
-
 It's not ready, so go away ;)
 
+Inspiration: https://dergigi.com/2023/01/19/how-to-build-a-nostr-gm-bot/
+
+# TODO
+
+- https://nostr.build
+
+# contribute
+
+- https://github.com/fiatjaf/noscl
+
+Try inbox feature: https://github.com/fiatjaf/noscl/commit/39f6db9c224121a69423505e75eb5acb77628d2b
+
+TODO: DM with zaps?  Hermmm....
+
+---
 
 # example feed
 
--- rss_to_nostr/__main__.py
+++ rss_to_nostr/__main__.py
@@ -1,11 +1,18 @@
 import os
 import subprocess
 import feedparser
+import json
 import time
+
+FETCH_INTERVAL = 60 * 10 # minutes
 
 def fetch_rss_feed(url):
     feed = feedparser.parse(url)
-    print(feed)
+    print(f"Fetching feed: {url}")
+
+    # print(f"{feed.keys()}")
+    # dict_keys(['bozo', 'entries', 'feed', 'headers', 'etag', 'href', 'status', 'encoding', 'version', 'namespaces'])
+
     return feed.entries
 
 def post_to_nostr(title, link):
@@ -17,31 +24,72 @@ def post_to_nostr(title, link):
 
 last_processed_article_id = None
 
+# def process_feed():
+#     global last_processed_article_id
+#     # rss_url = "https://www.theverge.com/rss/index.xml"
+#     rss_url = "https://www.theverge.com/reviews/rss/index.xml"
+#     entries = fetch_rss_feed(rss_url)
+
+#     # if last_processed_article_id is None:
+#     #     last_processed_article_id = entries[0].id
+
+#     for entry in entries:
+#         print(entry['title'], entry['link'])
+#     #     if entry.id == last_processed_article_id:
+#     #         break
+
+#     #     # post_to_nostr(entry.title, entry.link)
+#     #     print(f"${entry.title} - {entry.link}")
+
+#     # last_processed_article_id = entries[0].id
+
+
 def process_feed():
-    global last_processed_article_id
+    print("Processing feed...")
+    # Load the last_processed_articles from file
+    if os.path.exists("last_processed_articles.json"):
+        with open("last_processed_articles.json", "r") as f:
+            last_processed_articles = json.load(f)
+    else:
+        last_processed_articles = []
+
+    # rss_url = "https://www.theverge.com/reviews/rss/index.xml"
     rss_url = "https://www.theverge.com/rss/index.xml"
     entries = fetch_rss_feed(rss_url)
 
-    if last_processed_article_id is None:
-        last_processed_article_id = entries[0].id
+    entries_to_post = []
 
     for entry in entries:
-        print(entry)
-        if entry.id == last_processed_article_id:
-            break
+        if entry.id in last_processed_articles:
+            continue
 
-        # post_to_nostr(entry.title, entry.link)
-        print(f"${entry.title} - {entry.link}")
+        entries_to_post.append(entry)
 
-    last_processed_article_id = entries[0].id
+    for entry in entries_to_post:
+        post_to_nostr(entry.title, entry.link)
+        last_processed_articles.append(entry.id)
+
+    # Keep only the last 20 processed articles
+    last_processed_articles = last_processed_articles[-20:]
+
+    # Save the last_processed_articles to file
+    with open("last_processed_articles.json", "w") as f:
+        json.dump(last_processed_articles, f)
+
+
 
 if __name__ == "__main__":
-    process_feed()
+    while True:
+        process_feed()
+        time.sleep(FETCH_INTERVAL)
 
-# fetch_interval = 60  # minutes
-# schedule.every(fetch_interval).minutes.do(process_feed)
 
 # if __name__ == "__main__":
+#     # fetch_interval = 0.1  # minutes
+#     fetch_interval = 10  # minutes
+#     schedule.every(fetch_interval).minutes.do(process_feed)
+
+#     process_feed()
 #     while True:
 #         schedule.run_pending()
-#         time.sleep(1)
+#         time.sleep(60) # NOTE: a fetch_interval less than one minute won't work due to this
